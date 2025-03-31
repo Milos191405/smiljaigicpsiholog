0d586f3b86af62e695c8f2796c06ed6a25521198
Milos191405 <milos.mirkovic7@gmail.com>
2025-03-31 13:24:26 +0200

    fixing error

--- components/RandomQuote.js
+++ components/RandomQuote.js
@@ -1,23 +1,15 @@
-'use client'
+"use client";
 
 import { useState, useEffect } from "react";
 import quotes from "@/src/data/quotes.json";
 
 function RandomQuote() {
-  const [randomQuote, setRandomQuote] = useState({
-    text: "",
-    author: "",
-  });
+  const getRandomQuote = () => 
+    quotes.length > 0
+      ? quotes[Math.floor(Math.random() * quotes.length)]
+      : { text: "Psihoterapija je put do samorazumevanja.", author: "Nepoznat autor" };
 
-  const getRandomQuote = () => {
-    if (quotes.length > 0) {
-      return quotes[Math.floor(Math.random() * quotes.length)];
-    }
-    return {
-      text: "Psihoterapija je put do samorazumevanja.",
-      author: "Nepoznat autor",
-    };
-  };
+  const [randomQuote, setRandomQuote] = useState(getRandomQuote);
 
   useEffect(() => {
     setRandomQuote(getRandomQuote());
@@ -25,7 +17,7 @@ function RandomQuote() {
 
   return (
     <div className="p-4 text-center">
-      <p className="italic font-semibold">"{randomQuote.text}"</p>
+      <p className="italic font-semibold">{`"${randomQuote.text}"`}</p>
       <p className="italic">{randomQuote.author}</p>
     </div>
   );
