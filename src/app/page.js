b8bb7bab53b41e6efd0c2ea2e72a0baaf17eda12
Milos191405 <milos.mirkovic7@gmail.com>
2025-04-01 14:08:36 +0200

    fixing error

--- src/app/page.js
+++ src/app/page.js
@@ -10,46 +10,61 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      
+
       {/* Hero Section */}
       <section className="bg-gray-100 pt-[64px]">
-        <div className="mx-auto flex flex-col bg-background-secondary pt-10 pb-10 lg:flex-row items-center justify-between px-6 lg:px-20  ">
+        <div className="mx-auto flex flex-col bg-background-secondary pt-10 pb-10 lg:flex-row items-center justify-between px-6 lg:px-20">
           <div className="lg:w-1/2 text-center lg:text-left">
-            <h1 className="text-4xl lg:text-2xl font-semibold  leading-tight">
+            <h1 className="text-4xl lg:text-2xl font-semibold leading-tight">
               Terapija za decu, tinejdžere i njihove porodice
             </h1>
             <p className="mt-6 text-lg text-text-secondary">
               Pronađite podršku i razumevanje kroz stručni pristup.
             </p>
-            <Link 
-              href="/Kontakt" 
-              className="mt-6 inline-block bg-text-primary text-white px-8 py-4 text-lg rounded-full shadow-lg hover:bg-blue-700 transition">
+            <Link
+              href="/Kontakt"
+              className="mt-6 inline-block bg-text-primary text-white px-8 py-4 text-lg rounded-full shadow-lg hover:bg-blue-700 transition"
+            >
               Zakaži konsultaciju
             </Link>
           </div>
-          <div className="lg:w-1/ mt-10 lg:mt-0 flex justify-end">
+          <div className="lg:w-1/2 mt-10 lg:mt-0 flex justify-end">
             {/* Hero Image */}
-            <Image src="/Smiljka1.jpg" width={500} height={500} alt="Terapija" className="rounded-4xl shadow-lg" />
+            <Image
+              src="/Smiljka1.jpg"
+              width={500}
+              height={500}
+              alt="Terapija"
+              className="rounded-4xl shadow-lg"
+            />
           </div>
         </div>
       </section>
 
-     <div className="bg-text-primary text-background">
-      <RandomQuote />
+      <div className="bg-text-primary text-background">
+        <RandomQuote />
       </div>
 
       {/* About Section */}
       <section className="py-20 px-6 lg:px-20 bg-background">
         <div className="mx-auto flex flex-col lg:flex-row items-center">
           <div className="lg:w-1/2 flex justify-center">
-            <Image src="/Smiljka1.jpg" width={500} height={500} alt="Smilja Igic" className="rounded-lg shadow-lg text-text-primary" />
+            <Image
+              src="/Smiljka1.jpg"
+              width={500}
+              height={500}
+              alt="Smilja Igic"
+              className="rounded-4xl shadow-lg text-text-primary"
+            />
           </div>
           <div className="lg:w-1/2 lg:ml-10 mt-10 lg:mt-0">
             <h2 className="text-3xl font-semibold text-text-primary">O meni</h2>
             <p className="mt-6 text-gray-600 text-lg">
               Dipl. Psiholog Smilja Igic pruža podršku kroz terapijske procese, pomažući deci, tinejdžerima i porodicama da prevaziđu izazove.
             </p>
-            <Link href="/O_meni" className="mt-6 inline-block text-text-secondary hover:underline text-lg">Saznaj više</Link>
+            <Link href="/O_meni" className="mt-6 inline-block text-text-secondary hover:underline text-lg">
+              Saznaj više
+            </Link>
           </div>
         </div>
       </section>
@@ -57,14 +72,13 @@ export default function Home() {
       {/* Services Section */}
       <section className="py-20 bg-background-secondary px-6 lg:px-20">
         <div className="container mx-auto text-center">
-        <h2 className="text-3xl font-semibold text-gray-800">
-  <Link href="/Psihoterapija">Kako mogu pomoći?</Link>
-</h2>
+          <h2 className="text-3xl font-semibold text-gray-800">
+            <Link href="/Psihoterapija">Kako mogu pomoći?</Link>
+          </h2>
           <div className="mt-10 grid grid-cols-1 md:grid-cols-3 gap-10">
-
             <div className="p-8 bg-background-ternary text-background shadow-xl rounded-lg">
-              <h3 className="text-2xl font-bold ">Psihološko savetovanje</h3>
-              <p className="mt-4 ">Individualni pristup i profesionalna podrška u prevazilaženju životnih izazova.</p>
+              <h3 className="text-2xl font-bold">Psihološko savetovanje</h3>
+              <p className="mt-4">Individualni pristup i profesionalna podrška u prevazilaženju životnih izazova.</p>
             </div>
 
             <div className="p-8 bg-text-ternary text-background-secondary shadow-xl rounded-lg">
