412d7d27b57c3e474320289f99d34d910ff8ad1c
Milos191405 <milos.mirkovic7@gmail.com>
2025-10-22 12:57:37 +0200

    fixing images

--- components/ImageComponent.js
+++ components/ImageComponent.js
@@ -1,16 +1,21 @@
 import Image from "next/image";
 
-const ImageComponent = ({ src, alt, className = "", priority = false, width = 500, height = 400 }) => (
+const ImageComponent = ({
+  src,
+  alt,
+  className = "",
+  priority = false,
+  width = 500,
+  height = 400,
+}) => (
   <div className={`flex justify-center ${className}`}>
     <Image
       src={src}
       alt={alt}
       width={width}
       height={height}
-      sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
       className="rounded-4xl shadow-lg object-cover"
-      priority={priority} 
-      loading={priority ? "eager" : "lazy"}
+      priority={priority} // za LCP slike, postavi priority={true}
     />
   </div>
 );
